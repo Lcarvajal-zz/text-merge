43b56830b68696f3677bee9c42e913b1a78f9782
Omar Kadry <omar.kadry1@gmail.com>
2015-02-06 22:46:27 -0500

    Added some to load the groups

--- app/src/main/java/hophacks/omarkadry/textingmerge/TextMerge.java
+++ app/src/main/java/hophacks/omarkadry/textingmerge/TextMerge.java
@@ -1,17 +1,37 @@
 package hophacks.omarkadry.textingmerge;
 
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
 
 //Test
-public class TextMerge extends ActionBarActivity {
+public class TextMerge extends ActionBarActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    public final String DEBUG = "!!!DEBUG!!!";
+    private static final int LOADER_ID = 1;
+    Spinner groupSpinner;
+    SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_merge);
+
+        groupSpinner = (Spinner) findViewById(R.id.phoneGroups);
+
+        String[] groups = new String[]{ContactsContract.Groups.TITLE};
+        int[] to = new int[] { R.id.phoneGroups };
+        mAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null, groups, to, 0);
+        Log.i(DEBUG, "Attempting to set the Adapter");
+        groupSpinner.setAdapter(mAdapter);
     }
 
 
@@ -35,5 +55,26 @@ public class TextMerge extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    //Creates the GroupListLoader and returns it
+    public Loader onCreateLoader(int id, Bundle args) {
+        return new GroupListLoader(this);
+    }
+
+    @Override
+    //Swaps the loader into an adapter so the user can see the groups and select one
+    public void onLoadFinished(Loader loader, Cursor cursor) {
+        switch(loader.getId()){
+            case LOADER_ID:
+                mAdapter.swapCursor(cursor);
+                break;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
     }
 }
